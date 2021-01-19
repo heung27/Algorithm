c20ddbdf92c69bc64b8999e45840079a148f6741
Jin Heung Ahn <62068895+heung27@users.noreply.github.com>
2021-01-19 23:25:57 +0900

    2021. 01. 19
    쇠막대기

---
+++ solution/BOJ_10799/BOJ_10799.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ_10799 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = 0, result = 0;
+		String str = br.readLine();
+		for (int i=0; i<str.length(); i++) {
+			if (str.charAt(i) == '(') cnt++;
+			else {
+				cnt--;
+				if (str.charAt(i-1) == '(') result += cnt;
+				else result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
