3181004dd66a10e0891d55df7dd30e6a6b00f583
안진흥 <wlsgmd1535@naver.com>
2021-02-03 23:07:25 +0900

    2021. 02. 03

--- ssafy/algo/homework/SWEA_2001_파리_퇴치.java
+++ ssafy/algo/homework/SWEA_2001_파리_퇴치.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class SWEA_파리_퇴치 {
+public class SWEA_2001_파리_퇴치 {
 	public static int[][] map;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
