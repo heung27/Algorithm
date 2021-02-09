ff8a56e2437e1de83fdca62390cd77fedee9aa77
안진흥 <wlsgmd1535@naver.com>
2021-02-09 10:27:51 +0900

    2021. 02. 08 | SWEA | 암호문3

---
+++ solution/SWEA_1230_암호문3/README.md
@@ -0,0 +1,15 @@
+# 1230. 암호문3
+
+### solution
+* 입력 데이터를 리스트에 전부 add
+* 문자 I를 입력 받으면 x의 위치에 y개의 문자열 삽입
+* 문자 D를 입력 받으면 x의 위치의 문자열 삭제
+* 문자 A를 입력 받으면 리스트의 맨 뒤에 y개의 문자열 삽입
+* 문자열이 삽입,삭제 될 때 마다 x++하여 인덱스 조정
+
+### remind
+* List
+* LinkedList
+
+### link
+https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14zIwqAHwCFAYD
---
+++ solution/SWEA_1230_암호문3/SWEA_1230_암호문3.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class SWEA_1230_암호문3 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		for (int t=1; t<=10; t++) {
+			List<String> list = new LinkedList<>();
+			int len = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			while (len-- > 0) {
+				list.add(st.nextToken());
+			}
+			
+			int act = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
+			while (act-- > 0) {
+				char c = st.nextToken().charAt(0);
+				int x, y;
+				switch (c) {
+				case 'I':
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					while (y-- > 0) {
+						list.add(x++, st.nextToken());
+					}
+					break;
+				case 'D':
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					while (y-- > 0) {
+						list.remove(x++);
+					}
+					break;
+				case 'A':
+					y = Integer.parseInt(st.nextToken());
+					while (y-- > 0) {
+						list.add(st.nextToken());
+					}
+					break;
+				}
+			}
+			sb.append("#"+t);
+			for (int i=0; i<10; i++) {
+				sb.append(" "+list.get(i));
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
