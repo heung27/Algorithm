18d3f2c4f94d727f072cb83c76c6b9993117d51e
안진흥 <wlsgmd1535@naver.com>
2021-04-23 00:07:11 +0900

    2021. 04. 20 | BOJ | 미친 로봇

---
+++ BOJ/BOJ_1405_미친 로봇/BOJ_1405_미친_로봇.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_1405_미친_로봇 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	
+	static int[] movex = {0, 0, -1, 1};
+	static int[] movey = {1, -1, 0, 0};
+	
+	static double[] arr = new double[4];
+	
+	static boolean[][] check;
+	static int N;
+	static double answer;
+	
+	public static void main(String[] args) throws IOException {
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		int len = (N + 1) << 1;
+		
+		check = new boolean[len][len];
+		
+		for (int i=0; i<4; i++) {
+			arr[i] = Double.parseDouble(str[i+1]) * 0.01;
+		}
+		
+		check[N][N] = true;
+		DFS(0, N, N, 1.0);
+		
+		System.out.println(answer);
+	}
+	
+	static void DFS(int cnt, int x, int y, double sum) {
+		if (cnt == N) {
+			answer += sum;
+			return;
+		}
+		for (int d=0; d<4; d++) {
+			int nx = x + movex[d];
+			int ny = y + movey[d];
+			
+			if (check[nx][ny]) continue;
+			
+			check[nx][ny] = true;
+			DFS(cnt + 1, x + movex[d], y + movey[d], sum * arr[d]);
+			check[nx][ny] = false;
+		}
+	}
+}
