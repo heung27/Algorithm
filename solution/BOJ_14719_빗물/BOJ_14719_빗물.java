db6c8a5620f86fcf16c5ec1f37b792d522fecc9d
안진흥 <wlsgmd1535@naver.com>
2021-04-22 23:01:35 +0900

    2021. 04. 13 | BOJ | 빗물

---
+++ solution/BOJ_14719_빗물/BOJ_14719_빗물.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_14719_빗물 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int[] arr;
+	static int R, C, answer;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		arr = new int[C];
+	
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<C; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0, sum = 0;
+		for (int i=1; i<C; i++) {
+			if (arr[start] <= arr[i]) {
+				
+				answer += arr[start] * (i - start - 1) - sum;
+				start = i;
+				sum = 0;
+			}
+			else sum += arr[i];
+		}
+		
+		start = C-1; sum = 0;
+		for (int i=C-2; i>=0; i--) {
+			if (arr[start] < arr[i]) {
+				
+				answer += arr[start] * (start - i - 1) - sum;
+				start = i;
+				sum = 0;
+			}
+			else sum += arr[i];
+		}
+		
+		System.out.println(answer);
+	}
+}
