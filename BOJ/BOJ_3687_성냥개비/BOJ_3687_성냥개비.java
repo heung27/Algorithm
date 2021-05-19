80bb68bee21ea884c31881f77a1a0b2c396499ec
안진흥 <wlsgmd1535@naver.com>
2021-05-19 13:35:47 +0900

    2021. 04. 21 | BOJ | 성냥개비

---
+++ BOJ/BOJ_3687_성냥개비/BOJ_3687_성냥개비.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_3687_성냥개비 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	
+	static int[] num = {0, 0, 1, 7, 4, 2, 0, 8, 10};
+	
+	static long[] min = new long[101];
+	
+	public static void main(String[] args) throws IOException {
+		for (int i=1; i<9; i++) {
+			min[i] = num[i];
+		}
+		min[6] = 6;
+		
+		for (int i=9; i<101; i++) {
+			min[i] = min[i-2]*10 + num[2];
+			
+			for (int j=3; j<8; j++) {
+				min[i] = Math.min(min[i], min[i-j]*10 + num[j]);
+			}
+		}
+		
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(min[n]).append(" ");
+			
+			while (n != 0) {
+				if (n % 2 != 0) {
+					sb.append(7);
+					n -= 3;
+				}
+				else {
+					sb.append(1);
+					n -= 2;
+				}
+			}
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
