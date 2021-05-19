b84ba9d530722b062b90cf3533c55b3aded194c0
안진흥 <wlsgmd1535@naver.com>
2021-05-19 13:46:51 +0900

    2021. 04. 29 | Programmers | 메뉴 리뉴얼

---
+++ Programmers/Programmers_메뉴 리뉴얼/programmers_메뉴_리뉴얼.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class programmers_메뉴_리뉴얼 {
+	class Solution {
+	    ArrayList<String>[] arr;
+	    boolean[] list = new boolean[26];
+
+	    boolean[][] check;
+	    char[] temp;
+	    int[] max;
+
+	    public String[] solution(String[] orders, int[] course) {
+	        check = new boolean[orders.length][26];
+	        for (int i=0; i<orders.length; i++) {
+	            for (int j=0; j<orders[i].length(); j++) {
+	                check[i][orders[i].charAt(j)-'A'] = true;
+	                list[orders[i].charAt(j)-'A'] = true;
+	            }
+	        }
+
+	        temp = new char[course[course.length-1]];
+	        max = new int[course[course.length-1]];
+
+	        arr = new ArrayList[course[course.length-1]];
+	        for (int i=0; i<arr.length; i++) {
+	            arr[i] = new ArrayList<>();
+	        }
+
+	        DFS(0, 0, 0, course);
+
+
+	        for (int i=1; i<arr.length; i++) {
+	            for (String s : arr[i]) {
+	                arr[0].add(s);
+	            }
+	        }
+
+	        Collections.sort(arr[0]);
+
+	        String[] answer = new String[arr[0].size()];
+	        for (int i=0; i<arr[0].size(); i++) {
+	            answer[i] = arr[0].get(i);
+	        }
+	        return answer;
+	    }
+
+	    public void DFS(int cnt, int start, int course_idx, int[] course) {
+	        if (cnt == course[course_idx]) {
+	            int sum = 0;
+	            for (int i=0; i<check.length; i++) {
+	                boolean t = true;
+
+	                for (int j=0; j<cnt; j++) {
+	                    if (!check[i][temp[j]-'A']) {
+	                        t = false;
+	                        break;
+	                    }
+	                }
+
+	                if (t) sum++;
+	            }
+	            
+	            if (sum >= 2) {
+	                 if (max[course_idx] < sum) {
+	                arr[course_idx].clear();
+	                arr[course_idx].add(String.valueOf(temp, 0, cnt));
+	                max[course_idx] = sum;
+	                }
+	                else if (max[course_idx] == sum) {
+	                    arr[course_idx].add(String.valueOf(temp, 0, cnt));
+	                }
+	            }
+
+	            if (++course_idx == course.length) return;
+	        }
+	        for (int i=start; i<26; i++) {
+	            if (!list[i]) continue;
+
+	            temp[cnt] = (char)('A'+i);
+	            DFS(cnt+1, i+1, course_idx, course);
+	        }
+	    }
+	}
+}
