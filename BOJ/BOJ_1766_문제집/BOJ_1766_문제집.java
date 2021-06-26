97a9b83fee41ff8eac571c9c319153fb688c0753
안진흥 <wlsgmd1535@naver.com>
2021-06-26 18:34:40 +0900

    2021. 06. 26 | BOJ | 문제집

---
+++ BOJ/BOJ_1766_문제집/BOJ_1766_문제집.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_1766_문제집 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static class Node implements Comparable<Node> {
+		int n;
+		Node next;
+		
+		public Node(int n, Node next) {
+			this.n = n;
+			this.next = next;
+		}
+		
+		public Node(int n) {
+			this.n = n;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.n, o.n);
+		}
+	}
+	
+	static Node[] list;
+	static int[] in_degree;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		
+		list = new Node[N+1];
+		in_degree = new int[N+1];
+		
+		for (int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			list[a] = new Node(b, list[a]);
+			in_degree[b]++;
+		}
+		
+		topological_sort();
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	
+	static void topological_sort() {
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		
+		for (int i=1; i<=N; i++) {
+			if (in_degree[i] == 0) queue.offer(new Node(i));
+		}
+		
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();
+			
+			sb.append(node.n).append(" ");
+			
+			for (Node next = list[node.n]; next != null; next = next.next) {
+				if (--in_degree[next.n] == 0) queue.offer(new Node(next.n));
+			}
+		}
+	}
+}
