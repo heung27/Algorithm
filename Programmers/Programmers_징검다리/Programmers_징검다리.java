26e3dc64fd4f57b15a53e1dcbe797f1c55163fe9
안진흥 <wlsgmd1535@naver.com>
2021-06-29 00:19:56 +0900

    2021. 06. 28 | Programmers | 징검다리

---
+++ Programmers/Programmers_징검다리/Programmers_징검다리.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class Programmers_징검다리 {
+	
+	class Solution {
+	    int[] arr;
+		int N, len;
+	    
+	    public int solution(int distance, int[] rocks, int n) {
+	        Arrays.sort(rocks);
+	        
+	        N = n;
+	        len = rocks.length + 2;
+	        arr = new int[len];
+	        arr[len-1] = distance;
+	        
+	        for (int i=0; i<rocks.length; i++) {
+	            arr[i+1] = rocks[i];
+	        }
+
+	        return binarySearch(1, distance);
+	    }
+	    
+	    public int binarySearch(int left, int right) {
+	        while (left <= right) {
+	            int mid = (left + right) >> 1;
+	            if (check(mid)) {
+	            	left = mid + 1;
+	            }
+	            else {
+	            	right = mid - 1;
+	            }
+	        }
+	        return left - 1;
+	    }
+	    
+	    public boolean check(int k) {
+	    	int cnt = 0, pre = 0, post = 1;
+	        while (post < len) {
+	            if (arr[post] - arr[pre] < k) {
+	            	cnt++;
+	            }
+	            else {
+	            	pre = post;
+	            }
+	            post++;
+	        }
+	        return cnt <= N;
+	    }
+	}
+}
