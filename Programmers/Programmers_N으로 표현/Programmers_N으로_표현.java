931bb9540212d08300a8dde2b53bb7564ee585fb
안진흥 <wlsgmd1535@naver.com>
2021-09-20 00:01:48 +0900

    2021. 09. 18 | Programmers | N으로표현

---
+++ Programmers/Programmers_N으로 표현/Programmers_N으로_표현.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+	
+public class Programmers_N으로_표현 {
+	class Solution {
+	    public int solution(int N, int number) {
+	       int answer = -1;
+	        Set<Integer>[] setArr = new Set[9];
+	        int t = N;
+	        for(int i = 1; i < 9; i++) {
+	            setArr[i] = new HashSet<>();
+	            setArr[i].add(t);
+	            t = t * 10 + N;
+	        }
+	        for(int i = 1; i < 9; i++) {
+	            for(int j = 1; j < i; j++) {
+	                for(Integer a : setArr[j]) {
+	                    for(Integer b : setArr[i - j]) {
+	                        setArr[i].add(a + b);
+	                        setArr[i].add(a - b);
+	                        setArr[i].add(a * b);
+	                        if(b != 0) {
+	                            setArr[i].add(a / b);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        for(int i = 1; i < 9; i++) {
+	            if(setArr[i].contains(number)) {
+	                answer = i;
+	                break;
+	            }
+	        }
+	        return answer;
+	    }
+	}
+}
