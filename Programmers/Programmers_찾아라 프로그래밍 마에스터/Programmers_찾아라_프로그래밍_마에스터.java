7426d98c4969a586248258534164e2b1e01d64bf
Jin Heung An <wlsgmd1535@naver.com>
2021-09-25 01:29:48 +0900

    2021. 09. 25 | Programmers | 찾아라 프로그래밍 마에스터

---
+++ Programmers/Programmers_찾아라 프로그래밍 마에스터/Programmers_찾아라_프로그래밍_마에스터.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Programmers_찾아라_프로그래밍_마에스터 {
+	class Solution {
+	    public int solution(int[][] maps) {
+	        return BFS(maps);
+	    }
+	    
+	    public int BFS(int[][] maps) {
+	        int[] movex = {-1, 0, 1, 0};
+	        int[] movey = {0, 1, 0, -1};
+	        int n = maps.length;
+	        int m = maps[0].length;
+	        int cnt = 1;
+	        
+	        Queue<int[]> queue = new LinkedList<>();
+	        boolean[][] visit = new boolean[n][m];
+	        queue.offer(new int[] {0, 0});
+	        visit[0][0] = true;
+	        
+	        while (!queue.isEmpty()) {
+	            int size = queue.size();
+	            while (size-- > 0) {
+	                int[] pos = queue.poll();
+	                
+	                if (pos[0] == n-1 && pos[1] == m-1) return cnt;
+
+	                for (int d=0; d<4; d++) {
+	                    int nx = pos[0] + movex[d];
+	                    int ny = pos[1] + movey[d];
+
+	                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx][ny] == 0) continue;
+
+	                    if (!visit[nx][ny]) {
+	                        queue.offer(new int[]{nx, ny});
+	                        visit[nx][ny] = true;
+	                    }
+	                }
+	            }
+	            cnt++;
+	        }
+	        return -1;
+	    }
+	}
+}
