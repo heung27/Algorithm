17b6221cd877808585d7402d48f0615921e83013
안진흥 <wlsgmd1535@naver.com>
2021-12-12 23:56:08 +0900

    2021. 12. 12 | BOJ | 사전

---
+++ BOJ/BOJ_1256_사전/BOJ_1256_사전.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_1256_사전 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int N, M, K;
+	static int[][] dp;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		dp = new int[N+1][M+1];
+		
+		get(N, M, K);
+		
+		if (check(N, M) < K) {
+			System.out.println(-1);
+		} else {
+			System.out.println(sb.toString());
+		}
+	}
+	
+	public static int check(int a,int z){
+        if(a == 0 || z == 0) return 1;
+        if(dp[a][z] != 0) return dp[a][z];
+        return dp[a][z] = Math.min(check(a-1,z) + check(a,z-1), 1000000001);
+    }
+
+	public static void get(int a, int z, int k){
+        if(a == 0){
+            for(int i=0; i<z; i++) {
+            	sb.append("z");
+            }
+            return;
+        }
+        if(z == 0){
+            for(int i=0; i<a; i++) {
+            	sb.append("a");
+            }
+            return;
+        }
+
+        int check = check(a-1, z);
+
+        if(k > check){
+            sb.append("z");
+            get(a, z-1, k-check);
+        }
+        else{
+            sb.append("a");
+            get(a-1, z, k);
+        }
+    }
+}
